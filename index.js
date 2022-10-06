6f4fb1ba97dd04dd1a5ea6ff9d2b03065f96a15b
hudashk <shaikhhuda855@gmail.com>
2022-10-06 16:48:35 +0530

    upload new file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function validate(){
     var username=document.getElementById('username').value;
     var email=document.getElementById('email').value;
     var password=document.getElementById('password').value;
-    var contact=document.getElementById('contact').value;
+    var number=document.getElementById('number').value;
 
     if(name==null || name==""){
         document.getElementById('fname').innerHTML="Please enter your full name";
@@ -13,11 +13,38 @@ function validate(){
         document.getElementById('fname').innerHTML="";
     }
 
-    if(username==null || name==""){
+    if(username==null || username==""){
         document.getElementById('uname').innerHTML="Please enter your user name";
         return false;
     }
     else{
         document.getElementById('uname').innerHTML="";
     }
+
+    if(email==null || eamil==""){
+        document.getElementById('emailid').innerHTML="Please enter your email";
+        return false;
+    }
+    else{
+        document.getElementById('emailid').innerHTML="";
+    }
+
+    if(password==null || password==""){
+        document.getElementById('pass').innerHTML="Please enter your password";
+        return false;
+    }
+    else{
+        document.getElementById('pass').innerHTML="";
+    }
+
+    if(number==null || number==""){
+        document.getElementById('contactno').innerHTML="Please enter your contact";
+        return false;
+    }
+    else{
+        document.getElementById('contactno').innerHTML="";
+    }
+
+
+    
 }
